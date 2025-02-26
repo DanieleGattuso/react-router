497c76f3f9ffad3751166ce5d7582244263ea804
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-26 13:40:18 +0100

    almost done routing

---
+++ src/route/AboutPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header" // Importazione del componente Header
+
+// Componente per la pagina "Chi Siamo"
+export default function AboutPage() {
+    return (
+        <div className="container">
+            {/* Contenuto della pagina "Chi Siamo" */}
+            questa è la mia pagina chi siamo
+        </div>
+    )
+}
---
+++ src/route/AddPostPage.jsx
@@ -0,0 +1,12 @@
+import AddPost from "../components/AddPost";
+
+export default function AddPostPage() {
+
+    return (
+        <div className="container">
+            <h2>Inserisci un Nuovo articolo</h2>
+            <AddPost />
+        </div>
+    )
+
+}
